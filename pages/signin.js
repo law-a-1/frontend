c62e41911e337c32a1f5b60b60f9c6e946d472d1
zulfan <zulfandinca64@gmail.com>
2022-05-11 23:53:50 +0700

    add tailwind css styling for auth screen

--- pages/signin.js
+++ pages/signin.js
@@ -4,27 +4,29 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
             <Head>
                 <title>Login</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className={styles.main}>
-                <div>
-                    <div>
-                        <h2>Sign in</h2>
-                        <div>
-                            <input type="username" name="username" placeholder="username" />
+            <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+                <div className="bg-white rounded-2xl shadow-2xl flex max-w-4xl">
+                    <div className="p-10">
+                        <div className="py-10">
+                            <h2 className="text-3xl font-bold text-black-500 mb-2">Sign in to Account</h2>
+                            <div className="border-2 w-10 border-black-500 inline-block mb-2"></div>
+                        </div>
+                        <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                            <input type="username" name="username" placeholder="username" className="bg-gray-100 outline-none text-sm flex-1" />
+                        </div>
+                        <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                            <input type="password" name="password" placeholder="password" className="bg-gray-100 outline-none text-sm flex-1" />
                         </div>
                         <div>
-                            <input type="password" name="password" placeholder="password" />
-                        </div>
-                        <div>
-                            <a href="#">Sign In</a>
+                            <a href="#" className="border-2 border-black-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-white mb-3">Sign In</a>
                             <p>No Account? <a href="#">Create One</a></p>
                         </div>
-                        
                     </div>
                 </div>
             </main>
--- pages/signup.js
+++ pages/signup.js
@@ -10,23 +10,25 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className={styles.main}>
-                <div>
-                    <div>
-                        <h2>Sign Up an Account</h2>
-                        <div>
-                            <input type="username" name="username" placeholder="username" />
+            <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+                <div className="bg-white rounded-2xl shadow-2xl flex max-w-4xl">
+                    <div className="p-10">
+                        <div className="py-10">
+                            <h2 className="text-3xl font-bold text-black-500 mb-2">Sign Up</h2>
+                            <div className="border-2 w-10 border-black-500 inline-block mb-2"></div>
+                        </div>
+                        <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                            <input type="username" name="username" placeholder="username" className="bg-gray-100 outline-none text-sm flex-1" />
+                        </div>
+                        <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                            <input type="password" name="password" placeholder="password" className="bg-gray-100 outline-none text-sm flex-1" />
+                        </div>
+                        <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                            <input type="password" name="confirm-password" placeholder="confirm passowrd" className="bg-gray-100 outline-none text-sm flex-1" />
                         </div>
                         <div>
-                            <input type="password" name="password" placeholder="password" />
+                            <a href="#" className="border-2 border-black-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-white mb-3">Create Account</a>
                         </div>
-                        <div>
-                            <input type="password" name="confirm-password" placeholder="confirm password" />
-                        </div>
-                        <div>
-                            <a href="#">Create Account</a>
-                        </div>
-                        
                     </div>
                 </div>
             </main>
