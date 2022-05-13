ff92d7e57cff780a8814529258e51e796863f735
Teofanus Gary Setiawan <garyteofanus@gmail.com>
2022-05-13 18:09:55 +0700

    Add product add form page

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         <div className={styles.grid}>
           {[1, 2, 3, 4, 5, 6].map((e) => (
             <div className={styles.card} key={e}>
-              <Link href={`/${encodeURIComponent("product-slug")}`}>
+              <Link href={`${e}`}>
                 <a>
                   <Image
                     src="https://imgs.search.brave.com/Dygx-jBMHgi55RgTdfsZwV5kH_EcjWKBd9mUNveYuS4/rs:fit:1000:1000:1/g:ce/aHR0cHM6Ly9zMi5i/dWthbGFwYWsuY29t/L2ltZy8yMjMyODkx/OTAxL3ctMTAwMC9D/YW5na2lyX19fR2Vs/YXNfX19NdWdfQWph/aWJfR2FtYmFyX0Jp/c2FfQmVydWJhaF9T/ZXN1YWlfU3VodV8u/anBn"
