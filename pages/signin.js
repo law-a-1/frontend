e091253cd89ae3db5c4c61bfd09ebf5ac0ebe0cf
zulfan <zulfandinca64@gmail.com>
2022-05-13 15:33:44 +0700

    Integration auth-fe with auth-be

--- pages/signin.js
+++ pages/signin.js
@@ -1,8 +1,66 @@
+import React, {useEffect, useState} from 'react';
 import Head  from "next/head";
-import styles from '../styles/Home.module.css';
+import { useRouter } from 'next/router'
+
+export default function SignIn() {
+    const router = useRouter()
+
+    const [data, setData] = React.useState({
+        username : '',
+        password : '',
+    });
+    
+    const handleInputUsername= event => {
+        setData({
+            ... data,
+            username : event.target.value
+        });
+    };
+
+    const handleInputPassword = event => {
+        setData({
+            ... data,
+            password : event.target.value
+        });
+    };
+
+    const handleSignUp = event => {
+        router.push('http://localhost:3000/signup')
+    }
 
 
-export default function Home() {
+    const submit = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch('http://host-1906285516-port-55516.proxy.infralabs.cs.ui.ac.id/auth-service/login', {
+          method: 'POST',
+          credentials : 'include',
+          headers: {'Content-Type': 'application/json'},
+      
+          body: JSON.stringify({
+            "username": data.username,
+            "password" : data.password
+        })
+      }).then((response) => {
+        console.log(response)
+        if(response.ok){router.push('/logout')
+      } else {
+        if(data.username==''){
+          alert("Please enter a username!")
+        } 
+        else if (data.password==''){
+          alert("Please enter your password")
+        } else {
+          response.json().then(data => alert(data.detail))
+         }
+      }
+    
+    })
+        .catch(function(error) {
+                alert("Something went wrong")
+            })
+    };
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
             <Head>
@@ -12,21 +70,23 @@ export default function Home() {
 
             <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
                 <div className="bg-white rounded-2xl shadow-2xl flex max-w-4xl">
-                    <div className="p-10">
+                    <div className="p-10"> 
                         <div className="py-10">
                             <h2 className="text-3xl font-bold text-black-500 mb-2">Sign in to Account</h2>
                             <div className="border-2 w-10 border-black-500 inline-block mb-2"></div>
                         </div>
+                        <form method='post'>
                         <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
-                            <input type="username" name="username" placeholder="username" className="bg-gray-100 outline-none text-sm flex-1" />
+                            <input onChange={handleInputUsername} type="username" name="username" placeholder="username" className="bg-gray-100 outline-none text-sm flex-1" />
                         </div>
                         <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
-                            <input type="password" name="password" placeholder="password" className="bg-gray-100 outline-none text-sm flex-1" />
+                            <input onChange={handleInputPassword} type="password" name="password" placeholder="password" className="bg-gray-100 outline-none text-sm flex-1" />
                         </div>
                         <div>
-                            <a href="#" className="border-2 border-black-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-white mb-3">Sign In</a>
-                            <p>No Account? <a href="#">Create One</a></p>
+                            <button onClick={submit} className="border-2 border-black-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-white mb-3">Sign In</button>
                         </div>
+                        </form>
+                        <p>No Account? <a onClick={handleSignUp}>Create One</a> </p> 
                     </div>
                 </div>
             </main>
