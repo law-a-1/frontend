7ca4a0540efe04ec12ccff6d3246325177077043
Sae Pasomba <saepasomba12@gmail.com>
2022-05-14 11:13:22 +0700

    Fix className attribute clash

--- pages/add-product.js
+++ pages/add-product.js
@@ -4,15 +4,15 @@ import styles from "../styles/ProductForm.module.css";
 
 export default function AddProduct() {
   return (
-    <div classNameName={styles.container}>
+    <div className={styles.container}>
       <Head>
         <title>Add Product</title>
         <meta name="description" content="Add new product" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main classNameName={styles.main}>
-        <h1 classNameName={styles.title}>Add product</h1>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Add product</h1>
 
         <ProductForm method="post" />
       </main>
