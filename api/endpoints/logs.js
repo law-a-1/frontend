6791f727f19c6201c9878d377b5d63e0a2a7aa8e
hanifanggawi <hanifanggawi45@gmail.com>
2022-05-26 11:59:48 +0700

    feat: add pagination

--- api/endpoints/logs.js
+++ api/endpoints/logs.js
@@ -4,7 +4,7 @@ const BASE_URL = 'http://localhost:2323'
 export const LogsAPI = {
     getLogs : async (queryParams) => {
         const query = new URLSearchParams(queryParams)
-        const res = await fetch(`${BASE_URL}/logs${query}`)
+        const res = await fetch(`${BASE_URL}/logs?${query}`)
         const data = await res.json()
         return data
     },
--- pages/logs.js
+++ pages/logs.js
@@ -1,19 +1,28 @@
 import { useEffect, useState } from 'react'
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { LogsAPI } from '../api/endpoints/logs'
 import { LogItem } from '../components/LogPage/LogItem'
 
 export const Logs = () => {
   const [logs, setLogs] = useState()
+  const [query, setQuery] = useState({
+    page: 1,
+    page_size: 10
+  })
+
+  const changePage = (pageNumber) => {
+    setQuery({...query, page: pageNumber})
+  }
 
   useEffect(() => {
     async function fetchData() {
-      const logs = await LogsAPI.getLogs()
+      const logs = await LogsAPI.getLogs(query)
       setLogs(logs)
     }
     fetchData()
-  }, [])
+  }, [query])
 
-  return (
+  return logs && (
     <div className='page-container'>
         <h1 className='page-header'>Logs</h1>
         <table className='log-table'>
@@ -31,6 +40,27 @@ export const Logs = () => {
                 ))}
             </tbody>
         </table>
+        <div className='pagination-container'>
+          <div className="pagination">
+            <FaChevronLeft 
+              size={15}
+              onClick={() => changePage(query.page - 1)} 
+              style={{ 
+                visibility: (query.page > 1) ? 'visible' : 'hidden', 
+                cursor: 'pointer'
+              }} 
+            />
+            <span>{`Page ${query.page} of ${logs.pages}`}</span>
+            <FaChevronRight 
+              size={15} 
+              onClick={() => changePage(query.page + 1)} 
+              style={{ 
+                visibility: (query.page < logs.pages) ? 'visible' : 'hidden',
+                cursor: 'pointer'
+              }}
+            /> 
+          </div>        
+        </div>
     </div>
   )
 }
