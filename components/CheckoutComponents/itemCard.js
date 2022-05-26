f3ce70be179ebadfc22e9bf1bf56fcdedde71fa9
Sae Pasomba <saepasomba12@gmail.com>
2022-05-26 18:43:37 +0700

    Integrate cart to BE

---
+++ components/CheckoutComponents/itemCard.js
@@ -0,0 +1,67 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/cart.module.css'
+import React, {useEffect, useState} from 'react';
+
+export default function itemCard({item}) {
+    const [amount, setAmount] = useState(item.amount)
+    const [isRender, setRender] = useState(true)
+    const [totalPrice, setTotalPrice] = useState(item.total_price)
+
+    const token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MzYsImV4cCI6MTY1MzU2NTYzNiwiaWF0IjoxNjUzNTYyMDM2fQ.1QUOEEPYgB8FlbPDNwm0pQpxThYDk_4hBxz9E4s2cXo"
+    const edit_amount = async(product_id, is_add) => {
+
+        if (is_add) {
+            setAmount(amount+1)
+            setTotalPrice(totalPrice+item.product.price)
+        } else {
+            setAmount(amount-1)
+            setTotalPrice(totalPrice-item.product.price)
+        }
+
+        await fetch('http://localhost:8000/checkout/cart/', {
+                method: 'PUT',
+                body: JSON.stringify({"product_id": product_id, "is_add" : is_add}),
+                headers: {
+                    'Content-Type': "application/json",
+                    'Authorization' : token
+                }
+                // Cara dapet tokennya gimana?
+        })
+
+    }
+
+    const delete_item = async(product_id) => {
+
+        await fetch('http://localhost:8000/checkout/cart/', {
+                method: 'DELETE',
+                body: JSON.stringify({"product_id": product_id}),
+                headers: {
+                    'Content-Type': "application/json",
+                    'Authorization' : token
+                }
+                // Cara dapet tokennya gimana?
+        })
+        location.reload()
+    }
+
+    if (isRender) {
+        return (
+            <div className={`${styles.itemCard} drop-shadow-xl rounded-lg bg-[#f9fafb] container flex flex-row flex-wrap mx-auto w-10/12 sm:w-2/4 h-full my-3 px-5 py-2 col-sm-6`}>
+                <div className={`${styles.itemImage} basis-2/5 container`}>
+                    <img src={item.product.images} alt="Vercel Logo" layout="fill" objectFit="contain" />
+                </div>
+                <div className="itemInfo basis-3/5">
+                    <b>{item.product.name}</b>
+                    <p>Jumlah: {amount}</p>
+                    <button className="bg-[#22c55e] text-[#ffffff] rounded-full w-7 h-7 mr-5" onClick={() => edit_amount(item.product.product_id, true)}>+</button>
+                    <button className="bg-[#22c55e] text-[#ffffff] rounded-full w-7 h-7 mr-5" onClick={() => edit_amount(item.product.product_id, false)}>-</button>
+                    <p>Rp{totalPrice}</p>
+                    <button className="" onClick={() => delete_item(item.product.product_id)}><Image src="/bin.png" alt="bin image" width={24} height={24} /></button>
+                </div>
+            </div>
+        )
+    } else {
+        return
+    }
+}
--- pages/cart.js
+++ pages/cart.js
@@ -1,9 +1,46 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/cart.module.css'
+import ItemCard from '../components/CheckoutComponents/itemCard'
+
+import React, {useEffect, useState} from 'react';
+
 
 function Cart() {
-    return(
+    const [data, setData] = useState()
+    const [grandTotal, setGrandTotal] = useState(0)
+    const token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MzYsImV4cCI6MTY1MzU2NTYzNiwiaWF0IjoxNjUzNTYyMDM2fQ.1QUOEEPYgB8FlbPDNwm0pQpxThYDk_4hBxz9E4s2cXo"
+
+    const fetchData = async() => {
+        console.log("FETCHING DATA...")
+        const result = await fetch('http://localhost:8000/checkout/cart/', {
+            method: 'GET',
+            headers: {'Authorization' : token}
+            // Cara dapet tokennya gimana?
+        })
+        console.log("RESULT")
+        const result_data = await result.json()
+        console.log(result_data)
+        setData(result_data)
+        // setGrandTotal(result_data.grand_total)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const checkoutHandle = async() => {
+        await fetch('http://localhost:8000/checkout/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': "application/json",
+                    'Authorization' : token
+                }
+                // Cara dapet tokennya gimana?
+        })
+    }
+
+    return data && (
         <div>
             <Head>
                 <title>Cart</title>
@@ -13,55 +50,21 @@ function Cart() {
                 <h1 className="mx-auto font-bold text-2xl text-center">Keranjang</h1>
 
                 <div className="container itemList col">
-                    <div className={`${styles.itemCard} drop-shadow-xl rounded-lg bg-[#f9fafb] container flex flex-row flex-wrap mx-auto w-10/12 sm:w-2/4 h-full my-3 px-5 py-2 col-sm-6`}>
-                        <div className={`${styles.itemImage} basis-2/5 container`}>
-                            <Image src="/dummy-photo1.jpeg" alt="Vercel Logo" layout="fill" objectFit="contain" />
-                        </div>
-                        <div className="itemInfo basis-3/5">
-                            <b>Nama Barang Ceritanya</b>
-                            <p>Jumlah: 1</p>
-                            <button className="bg-[#22c55e] text-[#ffffff] rounded-full w-7 h-7 mr-5">+</button>
-                            <button className="bg-[#22c55e] text-[#ffffff] rounded-full w-7 h-7 mr-5">-</button>
-                            <p>Rp500.000</p>
-                            <button className=""><Image src="/bin.png" alt="bin image" width={24} height={24} /></button>
-                        </div>
-                    </div>
-
-                    <div className={`${styles.itemCard} drop-shadow-xl rounded-lg bg-[#f9fafb] container flex flex-row flex-wrap mx-auto w-10/12 sm:w-2/4 h-full my-3 px-5 py-2 col-sm-6`}>
-                        <div className={`${styles.itemImage} basis-2/5 container`}>
-                            <Image src="/dummy-photo2.png" alt="Vercel Logo" layout="fill" objectFit="contain" />
-                        </div>
-                        <div className="itemInfo basis-3/5">
-                            <b>Nama Barang Ceritanya</b>
-                            <p>Jumlah: 1</p>
-                            <button className="bg-[#22c55e] text-[#ffffff] rounded-full w-7 h-7 mr-5">+</button>
-                            <button className="bg-[#22c55e] text-[#ffffff] rounded-full w-7 h-7 mr-5">-</button>
-                            <p>Rp500.000</p>
-                            <button className=""><Image src="/bin.png" alt="bin image" width={24} height={24} /></button>
-                        </div>
-                    </div>
-
-                    <div className={`${styles.itemCard} drop-shadow-xl rounded-lg bg-[#f9fafb] container flex flex-row flex-wrap mx-auto w-10/12 sm:w-2/4 h-full my-3 px-5 py-2 col-sm-6`}>
-                        <div className={`${styles.itemImage} basis-2/5 container`}>
-                            <Image src="/dummy-photo1.jpeg" alt="Vercel Logo" layout="fill" objectFit="contain" />
-                        </div>
-                        <div className="itemInfo basis-3/5">
-                            <b>Nama Barang Ceritanya</b>
-                            <p>Jumlah: 1</p>
-                            <button className="bg-[#22c55e] text-[#ffffff] rounded-full w-7 h-7 mr-5">+</button>
-                            <button className="bg-[#22c55e] text-[#ffffff] rounded-full w-7 h-7 mr-5">-</button>
-                            <p>Rp500.000</p>
-                            <button className=""><Image src="/bin.png" alt="bin image" width={24} height={24} /></button>
-                        </div>
-                    </div>
+                    {
+                        data.items?.map((item, index) => {
+                            return (
+                                <ItemCard item={item} />
+                            )
+                        })
+                    }
                 </div>
 
                 <div className={`bg-[#f9fafb] drop-shadow-md container mx-auto w-3/4 md:w-1/4 h-full my-8 p-5 text-center rounded-lg col-sm-3`}>
-                    <p>Total Harga: <span className="font-bold">Rp2.500.000</span></p>
-                    <button className="bg-gradient-to-r from-green-500 to-cyan-500 text-[#ffffff] rounded-full p-2 w-4/5 my-2">Checkout</button>
+                    <p>Total Harga: <span className="font-bold">Rp{data.grand_total}</span></p>
+                    <button className="bg-[#e2e8f0] text-[#ffffff] rounded-full p-1 w-3/5 text-xs" onClick={() => location.reload()}>Update harga</button>
+                    <button className="bg-gradient-to-r from-green-500 to-cyan-500 text-[#ffffff] rounded-full p-2 w-4/5 my-2" onClick={() => checkoutHandle()}>Checkout</button>
                     <button className="bg-[#94a3b8] text-[#ffffff] rounded-full p-2 w-4/5 my-2">Batal</button>
                 </div>
-                
             </div>
         </div>
     )
